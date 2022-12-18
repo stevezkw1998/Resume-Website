e5541822e3ca20b88e58ad035f3ca17b46171221
unknown <steve1998kewen@gmail.com>
2022-12-17 21:04:10 -0500

    build

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ResumePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("resume/", ResumePageView.as_view(), name="resume"),
+    path("info/", ResumePageView.as_view(), name="info"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class HomePageView(TemplateView):
@@ -7,3 +8,9 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+class ResumePageView(TemplateView):
+    template_name = "pages/resume.html"
+
+def info(request):
+    return render(request, 'pages/resume.html', {'message': 1111111111111})
